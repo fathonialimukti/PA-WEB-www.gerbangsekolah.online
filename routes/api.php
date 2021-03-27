9b4c36b0f5e0852509aacf263b3339ecdf4ceaaa
fathoni ali mukti <fathonialimukti@it.student.pens.ac.id>
2021-03-27 13:11:41 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
-
 Route::post('login', 'Auth\AuthAPIController@login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
