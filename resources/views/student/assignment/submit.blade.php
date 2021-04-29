8dbb573b86b8152cdf327413725e3e5486b7c1d9
fathoni ali mukti <fathonialimukti@it.student.pens.ac.id>
2021-04-29 13:01:18 +0700

    update

--- resources/views/student/assignment/submit.blade.php
+++ resources/views/student/assignment/submit.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="table w-full mt-8 bg-white rounded">
-            <form action="{{ route('assignment.store-assignment',$AssignmentFile->assignment->id }}" method="post" class="w-full max-w-xl px-6 py-12" enctype="multipart/form-data">
+            <form action="{{ route('assignment.store-assignment',[$AssignmentFile->assignment->id]) }}" method="post" class="w-full max-w-xl px-6 py-12" enctype="multipart/form-data">
                 @csrf
                 <div class="md:flex md:items-center mb-6">
                     <div class="md:w-1/3">
